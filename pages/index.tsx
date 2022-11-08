b3e8a7c25b9995e07dde3c9fb4d3452d36330953
Erfan Ansari <dev.erfanansari@gmail.com>
2022-11-08 22:12:35 +0330

    refactor: remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,268 +2,213 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 import Calendar from 'react-calendar'
-import { format, formatDistance } from 'date-fns'
+import {
+  add,
+  eachHourOfInterval,
+  format,
+  formatDistance,
+  isEqual,
+  isToday,
+  startOfDay,
+  startOfToday,
+} from 'date-fns'
 
-type Duration =
-    | '1 hour'
-    | '2 hours'
-    | '3 hours'
-    | '4 hours'
-    | '5 hours'
-    | '6 hours'
-    | '7 hours'
-    | '8 hours'
-    | 'next workday'
+type Duration = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | 'next workday'
 
-// type Reservations = Record<string, { name: string; duration: string }[]>
 type Reservations = Record<string, { name: string; duration: Duration }[]>
 
 function isDuration(duration: string): duration is Duration {
-    return [
-        '1 hour',
-        '2 hours',
-        '3 hours',
-        '4 hours',
-        '5 hours',
-        '6 hours',
-        '7 hours',
-        '8 hours',
-        'next workday',
-    ].includes(duration)
+  return ['1', '2', '3', '4', '5', '6', '7', '8', 'next workday'].includes(
+    duration,
+  )
 }
 
 const Home: NextPage = () => {
-    const currentTime = new Date()
-    const [selectedDate, setSelectedDate] = useState<null | Date>(null)
-    const [name, setName] = useState('')
-    const [duration, setDuration] = useState<Duration>('1 hour')
+  const now = new Date()
 
-    const [hover, setHover] = useState(false)
-    const [reservations, setReservations] = useState<Reservations>({
-        '2022-11-09': [
-            {
-                name: 'John Doe',
-                duration: '1 hour',
-            },
-            {
-                name: 'Jane Doe',
-                duration: '2 hours',
-            },
-        ],
-    })
-    // const [workingHours, setWorkingHours] = useState([
-    //     { start: '09:00', end: '17:00' },
-    // ])
-    const workingHours = [
-        '09:00',
-        '10:00',
-        '11:00',
-        '12:00',
-        '13:00',
-        '14:00',
-        '15:00',
-        '16:00',
-        '17:00',
-    ]
+  const [selectedDate, setSelectedDate] = useState<null | Date>(null)
+  const [name, setName] = useState('')
+  const [duration, setDuration] = useState<Duration>('1')
+  const [hoveredDate, setHoveredDate] = useState<null | Date>(selectedDate)
+  const [hover, setHover] = useState(false)
 
-    const hoursLeftUntilWorkdayEnds = () => {
-        const now = new Date()
-        const today = format(now, 'yyyy-MM-dd')
-        const todayEnd = new Date(
-            `${today}T${workingHours[workingHours.length - 1]}:00`,
-        )
-        // return formatDistance(now, todayEnd).replace('about ', '')
-        const result = new Date(todayEnd).getTime() - new Date(now).getTime()
-        const hours = Math.floor(result / 1000 / 60 / 60)
-        const minutes = Math.floor((result / 1000 / 60 / 60 - hours) * 60)
-        // return `${hours} hours and ${minutes} minutes`
-        return [hours, minutes]
-    }
+  const [reservations, setReservations] = useState<Reservations>({
+    '2022-11-09': [
+      {
+        name: 'John Doe',
+        duration: '1',
+      },
+      {
+        name: 'Jane Doe',
+        duration: '2',
+      },
+    ],
+  })
 
-    console.log('hours left: ', hoursLeftUntilWorkdayEnds())
+  let workingHours = eachHourOfInterval({
+    start: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 9),
+    end: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 17),
+  }).map((h) => format(h, 'HH:mm'))
 
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        console.log({ name, duration })
-        if (selectedDate) {
-            const dateKey = format(selectedDate, 'yyyy-MM-dd')
-            const reservation = { name, duration }
-            if (reservations[dateKey]?.find((r) => r.name === reservation.name))
-                return
-            setReservations((prev) => ({
-                ...prev,
-                [dateKey]: [...(prev[dateKey] || []), reservation],
-            }))
-        }
-    }
+  const lastHour = workingHours[workingHours.length - 1]
+  const isPassedWorkingHours = now.getHours() >= parseInt(lastHour)
 
-    const inputDisabled = !selectedDate
-    const formattedDate = selectedDate ? format(selectedDate, 'yyyy-MM-dd') : ''
-    // console.log(date)
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log({ name, duration })
     if (selectedDate) {
-        // console.log(format(date, 'yyyy-MM-dd'))
-        console.log('HH:mm', format(currentTime, 'HH:mm'))
-        console.log('HH', format(currentTime, 'HH'))
+      const dateKey = format(selectedDate, 'yyyy-MM-dd')
+      const reservation = { name, duration }
 
-        console.log('date', selectedDate)
-        console.log('formattedDate', formattedDate)
-        // console.log(
-        //     'subHours',
-        //     format(subHours(currentTime, -workingHours[0]), 'HH:mm'),
-        // )
-        console.log(formatDistance(selectedDate, new Date()))
+      if (reservations[dateKey]?.find((r) => r.name === reservation.name))
+        return
+
+      setReservations((prev) => ({
+        ...prev,
+        [dateKey]: [...(prev[dateKey] || []), reservation],
+      }))
     }
+  }
 
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2">
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <header className="mt-14">
-                <h1 className="text-4xl lg:text-6xl font-bold">
-                    Welcome to the{' '}
-                    <p className="text-blue-600 sm:inline">Reservation App</p>
-                </h1>
-            </header>
-            <main className="flex w-full flex-1 flex-col items-center mt-16 md:px-20 text-center">
-                <form onSubmit={onSubmit} className="flex flex-col md:w-[50%]">
-                    <div className="mx-auto mb-8 flex min-h-[308px]">
-                        <Calendar
-                            className="border-1 border-gray-300 rounded-md"
-                            value={selectedDate}
-                            onChange={setSelectedDate}
-                            // formatDay={(_, date) => format(date, 'dd')}
-                            // formatDay={(_, date) => null}
-                            tileContent={({ date, view, activeStartDate }) => {
-                                if (view === 'month') {
-                                    const dateKey = format(date, 'yyyy-MM-dd')
-                                    const reservationsForDate =
-                                        reservations[dateKey]
-                                    const isNotActiveTile =
-                                        selectedDate &&
-                                        dateKey !==
-                                            format(selectedDate, 'yyyy-MM-dd')
-                                    if (
-                                        reservationsForDate?.length &&
-                                        isNotActiveTile
-                                    ) {
-                                        return (
-                                            <div
-                                                onMouseEnter={() =>
-                                                    setHover(true)
-                                                }
-                                                onMouseLeave={() =>
-                                                    setHover(false)
-                                                }
-                                                style={{
-                                                    height: '100%',
-                                                    width: '100%',
-                                                    position: 'absolute',
-                                                    top: 0,
-                                                    left: 0,
-                                                    display: 'flex',
-                                                    justifyContent: 'center',
-                                                    alignItems: 'center',
-                                                    backgroundColor: hover
-                                                        ? '#1087ff'
-                                                        : `rgba(0, 255, 0, ${
-                                                              reservationsForDate.length /
-                                                              10
-                                                          })`,
-                                                }}
-                                            >
-                                                <p
-                                                    className={`${
-                                                        hover
-                                                            ? 'block text-white'
-                                                            : 'hidden'
-                                                    } z-40`}
-                                                >
-                                                    {reservationsForDate.length}
-                                                    res
-                                                </p>
-                                            </div>
-                                        )
-                                    }
-                                    // return <p>{format(date, 'dd')}</p>
-                                }
-                                return null
-                            }}
-                            tileDisabled={({ date }) => {
-                                const isNotWorkday =
-                                    format(date, 'iiii') === 'Sunday' ||
-                                    format(date, 'iiii') === 'Saturday'
+  // start 9am and end 5pm
 
-                                const isPast =
-                                    date.getTime() <
-                                    new Date().getTime() - 1000 * 60 * 60 * 24
+  console.log('hours', workingHours)
 
-                                const isTodayReserver = !!reservations[
-                                    format(date, 'yyyy-MM-dd')
-                                ]?.some((r) => r.duration === 'next workday')
+  const inputDisabled = !selectedDate
+  const formattedDate = selectedDate ? format(selectedDate, 'yyyy-MM-dd') : ''
+  console.log('selectedDate', selectedDate)
+  if (selectedDate) {
+    const submittedDurations = reservations[formattedDate]
+      ?.map((r) => r.duration)
+      .reduce((acc, cur) => acc + parseInt(cur), 0)
+    console.log(submittedDurations)
+    workingHours = workingHours.slice(submittedDurations)
 
-                                return isNotWorkday || isTodayReserver || isPast
-                            }}
-                        />
-                    </div>
-                    <input
-                        type="text"
-                        className="border-2 border-gray-300 bg-white h-11 px-5 pr-16 rounded-lg text-md focus:outline-none
-                        disabled:opacity-50 disabled:cursor-not-allowed"
-                        disabled={inputDisabled}
-                        placeholder="Enter your name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <select
-                        name="duration"
-                        id="duration"
-                        className="mt-6 border-2 border-gray-300
-                      bg-white h-11 px-4 pr-16 rounded-lg text-md focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
-                        disabled={inputDisabled}
-                        value={duration}
-                        onChange={(e) => {
-                            if (isDuration(e.target.value)) {
-                                setDuration(e.target.value)
-                            }
+    const hoursLeft = formatDistance(now, selectedDate)
+    console.log(hoursLeft)
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="mt-14">
+        <h1 className="text-4xl lg:text-6xl font-bold">
+          Welcome to the{' '}
+          <p className="text-blue-600 sm:inline">Reservation App</p>
+        </h1>
+      </header>
+      <main className="flex w-full flex-1 flex-col items-center mt-16 md:px-20 text-center">
+        <form onSubmit={onSubmit} className="flex flex-col md:w-[50%]">
+          <div className="mx-auto mb-8 flex min-h-[308px]">
+            <Calendar
+              className="border-1 border-gray-300 rounded-md"
+              value={selectedDate}
+              onChange={setSelectedDate}
+              tileContent={({ date, view }) => {
+                if (view === 'month') {
+                  const dateKey = format(date, 'yyyy-MM-dd')
+                  const length = reservations[dateKey]?.length
+                  // const disabled = isPassedWorkingHours && isToday(date)
+                  const isTodayReserved = !!reservations[
+                    format(date, 'yyyy-MM-dd')
+                  ]?.some((r) => r.duration === 'next workday')
+
+                  if (length) {
+                    return (
+                      <div
+                        onMouseEnter={() => setHover(true)}
+                        onMouseLeave={() => setHover(false)}
+                        style={{
+                          backgroundColor: hover
+                            ? '#1087af'
+                            : isTodayReserved
+                            ? 'initial'
+                            : `rgba(0, 255, 0, ${length / 10})`,
                         }}
-                        placeholder="Enter duration"
-                    >
-                        {new Array(hoursLeftUntilWorkdayEnds()[0])
-                            .fill(0)
-                            .map((_, i) => {
-                                const hours = i + 1
-                                const text = `${hours} hour${
-                                    hours > 1 ? 's' : ''
-                                }`
-                                return (
-                                    <option key={hours} value={text}>
-                                        {text}
-                                    </option>
-                                )
-                            })}
-                        <option value="next workday">next workday</option>
-                    </select>
-                    <button
-                        disabled={!name || !duration}
-                        className="mt-4 bg-blue-500 transition-colors hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:hover:bg-blue-500 disabled:cursor-not-allowed"
-                    >
-                        Reserve
-                    </button>
-                </form>
-                <ul className="mt-4 text-left">
-                    {reservations[formattedDate]?.map((reservation) => (
-                        <li key={reservation.name}>
-                            <span className="mr-4 text-blue-600 font-bold">
-                                {`${reservation.name} - ${reservation.duration}`}
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </main>
-        </div>
-    )
+                        className="h-full w-full absolute top-0 left-0 flex items-center justify-center"
+                      >
+                        <p
+                          className={`${hover ? 'block text-white' : 'hidden'}`}
+                        >
+                          {length}
+                          res
+                        </p>
+                      </div>
+                    )
+                  }
+                }
+                return null
+              }}
+              tileDisabled={({ date }) => {
+                const isNotWorkday =
+                  format(date, 'iiii') === 'Sunday' ||
+                  format(date, 'iiii') === 'Saturday'
+
+                const isPast =
+                  date.getTime() <
+                  new Date().getTime() -
+                    (isPassedWorkingHours ? 0 : 1000 * 60 * 60 * 24)
+
+                const isTodayReserved = !!reservations[
+                  format(date, 'yyyy-MM-dd')
+                ]?.some((r) => r.duration === 'next workday')
+
+                return isNotWorkday || isTodayReserved || isPast
+              }}
+            />
+          </div>
+          <input
+            type="text"
+            className="border-2 border-gray-300 bg-white h-11 px-5 pr-16 rounded-lg text-md focus:outline-none
+                        disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={inputDisabled}
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <select
+            name="duration"
+            id="duration"
+            className="mt-6 border-2 border-gray-300
+                      bg-white h-11 px-4 pr-16 rounded-lg text-md focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={inputDisabled}
+            value={duration}
+            onChange={(e) => {
+              if (isDuration(e.target.value)) {
+                setDuration(e.target.value)
+              }
+            }}
+            placeholder="Enter duration"
+          >
+            {workingHours.map((_, i) => (
+              <option key={i} value={i + 1}>
+                {i + 1} hour{i > 0 && 's'}
+              </option>
+            ))}
+            <option value="next workday">next workday</option>
+          </select>
+          <button
+            disabled={!name || !duration}
+            className="mt-4 bg-blue-500 transition-colors hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:hover:bg-blue-500 disabled:cursor-not-allowed"
+          >
+            Reserve
+          </button>
+        </form>
+        <ul className="mt-4 text-left">
+          {reservations[formattedDate]?.map((reservation) => (
+            <li key={reservation.name}>
+              <span className="mr-4 text-blue-600 font-bold">
+                {`${reservation.name} - ${reservation.duration}`}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  )
 }
 
 export default Home
